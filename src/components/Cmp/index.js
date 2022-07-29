2a92b4fe4c7939d6fac942118d6507a45ab30e77
Zhenyu Shi <zhenyu.shi@compass.com>
2022-07-29 11:22:47 +0800

    feat: highlight of component

--- src/components/Cmp/index.js
+++ src/components/Cmp/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
+import cls from 'classnames';
+
+import styles from './index.less';
 
 export function Cmp({ cmp }) {
   const { style, value } = cmp;
-  
+
   return (
-    <div className={style.cmp} style={style}>
-      {value}
+    <div className={styles.main}>
+      {/* 组件本身 */}
+      <div className={style.cmp} style={style}>
+        {value}
+      </div>
+
+      {/* 组件的功能、选种样式 */}
+      <div
+        className={cls(styles.editStyle, styles.selected)}
+        style={{
+          top: style.top - 2,
+          left: style.left - 2,
+          width: style.width,
+          height: style.height,
+        }}
+      ></div>
     </div>
   );
 }
