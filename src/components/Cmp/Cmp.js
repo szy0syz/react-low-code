a80df6f074aec0bfac4016c74cb2c375b6cf6063
Zhenyu Shi <zhenyu.shi@compass.com>
2022-07-29 19:24:54 +0800

    feat: change size of element

--- src/components/Cmp/Cmp.js
+++ src/components/Cmp/Cmp.js
@@ -21,9 +21,65 @@ export class Cmp extends Component {
     this.context.setSelectedCmpIndex(index);
   };
 
+  // 伸缩组件 style top left width height
+  onMouseDown = (e) => {
+    const direction = e.target.dataset.direction;
+    if (!direction) {
+      return;
+    }
+    e.stopPropagation();
+    e.preventDefault();
+
+    let startX = e.pageX;
+    let startY = e.pageY;
+
+    const { cmp } = this.props;
+    const move = (e) => {
+      const x = e.pageX;
+      const y = e.pageY;
+
+      let disX = x - startX;
+      let disY = y - startY;
+
+      // style top left width height
+      let newStyle = {};
+      // todo top left
+      if (direction) {
+        if (direction.indexOf('top') >= 0) {
+          disY = 0 - disY;
+          newStyle.top = cmp.style.top - disY;
+        }
+
+        if (direction.indexOf('left') >= 0) {
+          disX = 0 - disX;
+          newStyle.left = cmp.style.left - disX;
+        }
+      }
+
+      Object.assign(newStyle, {
+        width: cmp.style.width + disX,
+        height: cmp.style.height + disY,
+      });
+
+      this.context.updateSelectedCmp(newStyle);
+
+      startX = x;
+      startY = y;
+    };
+
+    const up = () => {
+      document.removeEventListener('mousemove', move);
+      document.removeEventListener('mouseup', up);
+    };
+    document.addEventListener('mousemove', move);
+    document.addEventListener('mouseup', up);
+  };
+
   render() {
     const { cmp, selected } = this.props;
     const { style, value } = cmp;
+
+    const { width, height } = style;
 
     return (
       <div
@@ -49,7 +105,70 @@ export class Cmp extends Component {
             width: style.width,
             height: style.height,
           }}
-        ></ul>
+          onMouseDown={this.onMouseDown}
+        >
+          <li
+            className={styles.stretchDot}
+            style={{ top: -8, left: -8 }}
+            data-direction="top, left"
+          />
+
+          <li
+            className={styles.stretchDot}
+            style={{
+              top: -8,
+              left: width / 2 - 8,
+            }}
+            data-direction="top"
+          />
+
+          <li
+            className={styles.stretchDot}
+            style={{ top: -8, left: width - 8 }}
+            data-direction="top right"
+          />
+
+          <li
+            className={styles.stretchDot}
+            style={{ top: height / 2 - 8, left: width - 8 }}
+            data-direction="right"
+          />
+
+          <li
+            className={styles.stretchDot}
+            style={{
+              top: height - 8,
+              left: width - 8,
+            }}
+            data-direction="bottom right"
+          />
+
+          <li
+            className={styles.stretchDot}
+            style={{
+              top: height - 8,
+              left: width / 2 - 8,
+            }}
+            data-direction="bottom"
+          />
+
+          <li
+            className={styles.stretchDot}
+            style={{
+              top: height - 8,
+              left: -8,
+            }}
+            data-direction="bottom left"
+          />
+          <li
+            className={styles.stretchDot}
+            style={{
+              top: height / 2 - 8,
+              left: -8,
+            }}
+            data-direction="left"
+          />
+        </ul>
       </div>
     );
   }
