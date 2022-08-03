51d8bd4d75f78b00a84d1b83bc604c81f115de49
Zhenyu Shi <zhenyu.shi@compass.com>
2022-08-03 13:39:00 +0800

    chore: config

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,14 @@
 // * 配置完成后记得重启下
 const CracoLessPlugin = require("craco-less");
+const path = require("path");
+const pathResolve = (pathUrl) => path.join(__dirname, pathUrl);
 
 module.exports = {
+  webpack: {
+    alias: {
+      "@": pathResolve("src"),
+    },
+  },
   babel: {
     //用来支持装饰器
     plugins: [["@babel/plugin-proposal-decorators", { legacy: true }]],
